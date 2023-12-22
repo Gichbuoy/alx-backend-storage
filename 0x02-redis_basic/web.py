2edc8ca2472de08744774fe8502ef53727479a3a
Alex Maina <lexizgichbuoy@gmail.com>
2023-12-22 08:17:14 +0000

    web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,19 +3,61 @@
 Implementing get_page() in a new file
 """
 
-from typing import Callable
+import requests
+import redis
+import time
 from functools import wraps
+from typing import Callable
+
+# Create a Redis client
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 
-def count_requests(method: Callable) -> Callable:
+def cache_result(expiration_time=10):
     """
-    Track how many times a url has been accessed
+    Decorator to cache the result of a function with a specified
+    expiration time.
     """
-    @wraps(method)
-    def wrapper(self, url: str) -> str:
-        """
-        Wrapper function
-        """
-        self._redis.incr(f"count:{url}")
-        return method(self, url)
-    return wrapper
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            # Generate a key based on function name and arguments
+            key = f"{func.__name__}:{args}"
+
+            # Check if the result is in the cache
+            cached_result = redis_client.get(key)
+            if cached_result:
+                return cached_result.decode('utf-8')
+
+            # If not in cache, call the original function
+            result = func(*args, **kwargs)
+
+            # Store the result in the cache with the specified expiration time
+            redis_client.setex(key, expiration_time, result)
+
+            return result
+        return wrapper
+    return decorator
+
+
+@cache_result()
+def get_page(url: str) -> str:
+    """
+    Retrieves the HTML content of a URL and caches the result.
+
+    Args:
+        url (str): The URL to fetch the HTML content from.
+
+    Returns:
+        str: The HTML content of the URL.
+    """
+    # Simulate a slow response for testing purposes
+    response = requests.get(url)
+    time.sleep(3)  # Simulating a 3-second delay
+    return response.text
+
+
+# Example usage
+url = "http://slowwly.robertomurray.co.uk/delay/3000/url/http://www.google.com"
+html_content = get_page(url)
+print(html_content)
